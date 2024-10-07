ade7304a2fe9e43a59a8f1322e6e9778b86ea876
mch-fauzy <mucha.fauzy@gmail.com>
2024-10-07 16:00:06 +0700

    fix: remove expected error on startup

--- server.ts
+++ server.ts
@@ -4,11 +4,6 @@ import { checkDbConnection, checkRedisConnection } from './utils/connection-chec
 import { logger } from './configs/winston';
 import http from 'http';
 
-// Define a custom error type to include the 'code' property
-interface ServerError extends Error {
-    code?: string;
-}
-
 const PORT = CONFIG.SERVER.PORT;
 const app = express();
 app.use(express.json());
@@ -24,12 +19,8 @@ Promise.all([checkDbConnection(), checkRedisConnection()])
         });
 
         // Handle startup errors
-        server.on('error', (error: ServerError) => {
-            if (error.code === 'EADDRINUSE') {
-                logger.error(`[server] Port ${PORT} is already in use. Please choose a different port.`);
-            } else {
-                logger.error(`[server] An error occurred while starting the server: ${error.message}`);
-            }
+        server.on('error', (error) => {
+            logger.error(`[server] An error occurred while starting the server: ${error.message}`);
             process.exit(1);
         });
     });
