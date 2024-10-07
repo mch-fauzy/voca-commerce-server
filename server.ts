2ebf9dd61e44d33c30dde1a9827d73e5b4aec6a7
mch-fauzy <mucha.fauzy@gmail.com>
2024-10-07 16:05:46 +0700

    fix: use named import for express and http

--- server.ts
+++ server.ts
@@ -1,18 +1,19 @@
-import express from 'express';
+import express, { json } from 'express';
+import { createServer } from 'http';
+
 import { CONFIG } from './configs/config';
 import { checkDbConnection, checkRedisConnection } from './utils/connection-check';
 import { logger } from './configs/winston';
-import http from 'http';
 
 const PORT = CONFIG.SERVER.PORT;
 const app = express();
-app.use(express.json());
+app.use(json());
 
 // Wait for both database and Redis connection
 Promise.all([checkDbConnection(), checkRedisConnection()])
     .then(() => {
         // Create an HTTP server instance from the Express app
-        const server = http.createServer(app);
+        const server = createServer(app);
 
         server.listen(PORT, () => {
             logger.info(`Server is running on port ${PORT}`);
